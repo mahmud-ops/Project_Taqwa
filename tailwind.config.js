dfde59b760952c86f42502a823fe6b623bc1b6ec
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-09-02 21:45:23 +0600

    Added roadmap

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
